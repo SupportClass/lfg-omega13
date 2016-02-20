1e9a87e356738c4acd6915132ee17f87ea86d9af
Alex Van Camp <email@alexvan.camp>
2016-02-20 12:29:02 -0600

    feat(configschema): add config validation via a configschema.json

--- extension.js
+++ extension.js
@@ -4,22 +4,6 @@ var request = require('request');
 var querystring = require('querystring');
 
 module.exports = function (nodecg) {
-	if (!nodecg.bundleConfig) {
-		nodecg.log.error('cfg/lfg-omega13.json was not found. ' +
-			'This file is where the Pushover application and user keys are set.');
-		return;
-	}
-	if (typeof nodecg.bundleConfig.appToken === 'undefined') {
-		nodecg.log.error('"appToken" is not defined in cfg/lfg-omega13.json! ' +
-			'This token (obtained from your Pushover application dashboard) is required to send notifications.');
-		return;
-	} else if (typeof nodecg.bundleConfig.deliveryToken === 'undefined') {
-		nodecg.log.error('"deliveryToken" is not defined in cfg/lfg-omega13.json! ' +
-			'This token (obtained from your Pushover profile) defines what user or group to ' +
-			'send notification to, and is required.');
-		return;
-	}
-
 	nodecg.listenFor('help', function (message, cb) {
 		request.post({
 			url: 'https://api.pushover.net/1/messages.json',
